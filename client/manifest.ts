83c9b275e8b0269a93d3afca9d1be0b2b3ee7c9d
melvinchia3636 <melvinchia623600@gmail.com>
2025-11-15 21:08:14 +0800

    fix: fix routes config to match new API

--- client/manifest.ts
+++ client/manifest.ts
@@ -5,7 +5,7 @@ export default {
   name: 'Currency Converter',
   icon: 'tabler:currency-dollar',
   routes: {
-    'currency-converter': lazy(() => import('@'))
+    '/': lazy(() => import('@'))
   },
   category: 'Utilities'
 } satisfies ModuleConfig
