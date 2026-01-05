934e4ded3f07b33aacebf21b753f352f90ce7714
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:58:27 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Currency Converter',
-  icon: 'tabler:currency-dollar',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Utilities'
 } satisfies ModuleConfig
